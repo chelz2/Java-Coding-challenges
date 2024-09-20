3d99908256c915b4b4659d83f885c6beb380196c
chelz2 <golirida@hotmail.com>
2024-09-20 14:11:58 +0530

    printing string o/p  in java -- initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * Everything is wriiten in class in java
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World in Java");
+        
+        // challenge printing - First Two Letter of our name
+         
+        System.out.println(" GGG   RRRR ");
+        System.out.println("G   G  R   R");
+        System.out.println("G      R   R");
+        System.out.println("G GGG  RRRR ");
+        System.out.println("G   G  R R  ");
+        System.out.println("G   G  R  R");
+        System.out.println(" GGG   R   R");
+    }
+}
+
+
