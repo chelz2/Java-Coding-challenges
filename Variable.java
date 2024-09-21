8e162bc3e08d4ff2b67c0a00e0fafb0a311012de
chelz2 <golirida@hotmail.com>
2024-09-21 18:11:12 +0530

    chore : variables & dtypes in java

---
+++ Variable.java
@@ -0,0 +1,23 @@
+/**
+ * Variable & data-tpyes in java - int, long, double, string, char 
+ * Math operations in java
+ * 
+ */
+public class Variable {
+    public static void main(String[] args) {
+        // int data type
+        int apple = 5;
+        System.out.println(apple);
+
+        // string 
+        String first_Name = "Man";
+        String last_Name = "Hoe";
+        String full_Name = "full name is " + first_Name + last_Name;
+
+        System.out.println(full_Name);
+
+        // char
+        char mathGrade = 'A';
+        System.out.println("Math Grade: " + mathGrade);
+    }
+}
